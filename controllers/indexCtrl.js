44d564249ecf96fed21032bae6d17e5badfc8445
uzoeddie <uo@sms-digital.com>
2021-11-28 16:12:43 +0100

    changed render file path

--- controllers/indexCtrl.js
+++ controllers/indexCtrl.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index.ejs', { root: './views' });
 });
 
 router.get('/terms-of-service', (req, res) => {
-    res.render('terms');
+    res.render('terms', { root: './views' });
 });
 
 router.get('/privacy-policy', (req, res) => {
-    res.render('privacy');
+    res.render('privacy', { root: './views' });
 });
 
 module.exports = router;
