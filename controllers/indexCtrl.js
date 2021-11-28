8536da5086a4ddd54b2ef6e07c808385c7f08ec0
uzoeddie <uo@sms-digital.com>
2021-11-28 15:54:45 +0100

    change path of views

--- controllers/indexCtrl.js
+++ controllers/indexCtrl.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('/var/task/views/index');
 });
 
 router.get('/terms-of-service', (req, res) => {
-    res.render('terms');
+    res.render('/var/task/views/terms');
 });
 
 router.get('/privacy-policy', (req, res) => {
-    res.render('privacy');
+    res.render('/var/task/views/privacy');
 });
 
 module.exports = router;
