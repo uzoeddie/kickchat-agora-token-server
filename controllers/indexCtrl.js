ea184a8a16100e0c06cfa4ad63815ef99edf329c
uzoeddie <uo@sms-digital.com>
2021-11-28 16:14:45 +0100

    change path

--- controllers/indexCtrl.js
+++ controllers/indexCtrl.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.ejs', { root: './views' });
+    res.render('index.ejs', { root: './var/task/views' });
 });
 
 router.get('/terms-of-service', (req, res) => {
-    res.render('terms', { root: './views' });
+    res.render('terms.ejs', { root: './var/task/views' });
 });
 
 router.get('/privacy-policy', (req, res) => {
-    res.render('privacy', { root: './views' });
+    res.render('privacy.ejs', { root: './var/task/views' });
 });
 
 module.exports = router;
