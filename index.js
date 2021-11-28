30cae857a071c2b8220e18934d17a5689f728cb4
uzoeddie <uo@sms-digital.com>
2021-11-28 02:30:05 +0100

    moved service account file to root

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const helmet = require("helmet");
 const PORT = 8080;
 
 if(process.env.NODE_ENV === 'development') {
-    const serviceAccount = require('./config/kickchatdev-service-account.json');
+    const serviceAccount = require('./kickchatdev-service-account.json');
     adminInitializeApp(serviceAccount);
 } else {
     let serviceData = {
@@ -24,8 +24,8 @@ if(process.env.NODE_ENV === 'development') {
         "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
         "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
     };
-    fs.writeFileSync('./config/kickchat-service-account.json', JSON.stringify(serviceData), 'utf8');
-    const serviceAccountData = require('./config/kickchat-service-account.json');
+    fs.writeFileSync('./kickchat-service-account.json', JSON.stringify(serviceData), 'utf8');
+    const serviceAccountData = require('./kickchat-service-account.json');
     adminInitializeApp(serviceAccountData);
 }
 
