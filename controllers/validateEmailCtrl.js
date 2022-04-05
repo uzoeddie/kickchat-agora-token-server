e65054702b86eeb71612ed772cae514ef0f9c51d
uzoeddie <uo@sms-digital.com>
2022-04-05 23:58:11 +0200

    replaced deep-email-validator with mailchecker api

--- controllers/validateEmailCtrl.js
+++ controllers/validateEmailCtrl.js
@@ -1,8 +1,49 @@
-const emailValidator = require('deep-email-validator');
+const axios = require('axios');
 
 module.exports = {
     async validateEmail(req, res) {
-        const response = await emailValidator.validate(req.query.email);
-        return res.json(response);
+        try {
+            const domain = req.query.email.split('@')[1];
+            const lookup = await axios(requestOptions(domain));
+            let response = {};
+            if (lookup.data !== null && lookup.data.valid && !lookup.data.disposable && !lookup.data.block) {
+                response = {
+                    valid: true,
+                    validators: {
+                        regex: { valid: true },
+                        typo: { valid: true },
+                        disposable: { valid: true },
+                        mx: { valid: true },
+                        smtp: { valid: true }
+                    }
+                }
+            } else {
+                response = {
+                    valid: false,
+                    validators: {
+                        regex: { valid: false },
+                        typo: { valid: false },
+                        disposable: { valid: false },
+                        mx: { valid: false },
+                        smtp: { valid: false }
+                    }
+                }
+            }
+            return res.json(response);
+        } catch (error) {
+            console.log(error);
+        }
     },
+}
+
+function requestOptions(domain) {
+    return {
+        method: 'GET',
+        url: 'https://mailcheck.p.rapidapi.com/',
+        params: { domain },
+        headers: {
+          'X-RapidAPI-Host': 'mailcheck.p.rapidapi.com',
+          'X-RapidAPI-Key': process.env.EMAIL_VERIFICATION_API
+        }
+    };
 }
