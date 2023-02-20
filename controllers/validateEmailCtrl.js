48685edece2d34e07013fc0063e902c6f7ef6246
uzoeddie <uo@sms-digital.com>
2023-02-20 20:59:46 +0100

    enable email validation

--- controllers/validateEmailCtrl.js
+++ controllers/validateEmailCtrl.js
@@ -4,41 +4,31 @@ module.exports = {
     async validateEmail(req, res) {
         try {
             const domain = req.query.email.split('@')[1];
-            // const lookup = await axios(requestOptions(domain));
-            // let response = {};
-            let response = {
-                valid: true,
-                validators: {
-                    regex: { valid: true },
-                    typo: { valid: true },
-                    disposable: { valid: true },
-                    mx: { valid: true },
-                    smtp: { valid: true }
+            const lookup = await axios(requestOptions(domain));
+            let response = {};
+            if (lookup.data !== null && lookup.data.valid && !lookup.data.disposable && !lookup.data.block) {
+                response = {
+                    valid: true,
+                    validators: {
+                        regex: { valid: true },
+                        typo: { valid: true },
+                        disposable: { valid: true },
+                        mx: { valid: true },
+                        smtp: { valid: true }
+                    }
+                }
+            } else {
+                response = {
+                    valid: false,
+                    validators: {
+                        regex: { valid: false },
+                        typo: { valid: false },
+                        disposable: { valid: false },
+                        mx: { valid: false },
+                        smtp: { valid: false }
+                    }
                 }
             }
-            // if (lookup.data !== null && lookup.data.valid && !lookup.data.disposable && !lookup.data.block) {
-            //     response = {
-            //         valid: true,
-            //         validators: {
-            //             regex: { valid: true },
-            //             typo: { valid: true },
-            //             disposable: { valid: true },
-            //             mx: { valid: true },
-            //             smtp: { valid: true }
-            //         }
-            //     }
-            // } else {
-            //     response = {
-            //         valid: false,
-            //         validators: {
-            //             regex: { valid: false },
-            //             typo: { valid: false },
-            //             disposable: { valid: false },
-            //             mx: { valid: false },
-            //             smtp: { valid: false }
-            //         }
-            //     }
-            // }
             return res.json(response);
         } catch (error) {
             console.log(error);
