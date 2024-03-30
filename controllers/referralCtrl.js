cbb7a740d19e5b7b465f83000759ed08b921d6b2
uzochukwueddie <uzo.odozi@gmail.com>
2024-03-30 21:01:04 +0100

    added create branch controller

--- controllers/referralCtrl.js
+++ controllers/referralCtrl.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
 const { FieldValue } = require('firebase-admin/firestore');
+const axios = require('axios');
 
 const ADMINS = 'referrals';
 
@@ -16,4 +17,50 @@ module.exports = {
             return res.json(error);
         }
     },
+
+    async readBranchDeepLink(req, res) {
+        try {
+            const { deepLink } = req.params;
+            const getUrl = `https://api2.branch.io/v1/url?url=${deepLink}&branch_key=${process.env.BRANCH_KEY}`;
+            const response = await axios.get(getUrl, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            });
+            return res.json({message: 'Branch deep link', url: response.data.data.url});
+
+        } catch (error) {
+            return res.json(error);
+        }
+    },
+
+    async createBranchDeepLink(req, res) {
+        try {
+            const { alias } = req.body;
+            const url = 'https://api2.branch.io/v1/url';
+            const response = await axios.post(url, {
+                branch_key: process.env.BRANCH_KEY,
+                channel: "kickchat",
+                alias,
+                feature: "referral",
+                data: {
+                    referral_key: alias,
+                    $og_title: "KickChat",
+                    $og_description: "Connect with fans of your favorite teams that share the same interests as you. Download the app to get started.",
+                    $og_image_url: "https://res.cloudinary.com/soccerkik/image/upload/v1641150827/icon.png",
+                    $desktop_url: "https://kickchat.download"
+                }
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                }
+            });
+            return res.json({message: 'Branch deep link created', url: response.data.url});
+
+        } catch (error) {
+            return res.json(error);
+        }
+    }
 }
--- routes/referral.js
+++ routes/referral.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const adminCtrl = require('../controllers/referralCtrl');
+const referralCtrl = require('../controllers/referralCtrl');
 
-router.post('/update_referral_count', adminCtrl.updateReferralCount);
+router.get('/read_branch_deep_link/:deepLink', referralCtrl.readBranchDeepLink);
+router.post('/create_branch_deep_link', referralCtrl.createBranchDeepLink);
+router.post('/update_referral_count', referralCtrl.updateReferralCount);
 
 module.exports = router;
