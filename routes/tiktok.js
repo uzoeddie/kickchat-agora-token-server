cff059539f33ac67128a29ca732a621adce70973
uzochukwueddie <uzo.odozi@gmail.com>
2026-08-08 21:19:14 +0200

    update apple app site association file

--- routes/tiktok.js
+++ routes/tiktok.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const tiktokCtrl = require("../controllers/tiktokCtrl");
 
+// Mobile
 router.get("/auth/tiktok/callback", tiktokCtrl.tikTokRedirect);
+// Web
 router.get("/auth/tiktok/web", tiktokCtrl.tikTokWebAuthorization);
 router.get("/auth/tiktok/web/callback", tiktokCtrl.tikTokWebCallback);
 
+// Mobile & Web
 router.post("/check_user_by_tiktok", tiktokCtrl.checkIfTikTokUserExists);
 router.post("/create_user_with_tiktok", tiktokCtrl.createUserWithTikTok);
 router.post("/auth/tiktok", tiktokCtrl.tikTokAuthentication);
